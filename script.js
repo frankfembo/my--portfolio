e8e4695b21667c6238826d7066c3677f3f3f4b67
Franktembo <tembofrank600@gmail.com>
2022-07-26 11:13:14 +0200

    Add dropdown page & set it to show only onclick of event

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function crossMenuFunc(x) {
   x.classList.toggle("change");
 }
+
+function dropdownFunc() {
+  document.getElementById("menuDrop").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.nav-menu')) {
+    let drpList = document.getElementsByClassName("drop-list");
+
+    for (let i = 0; i < drpList.length; i++) {
+      let listOpen = drpList[i];
+
+      if (listOpen.classList.contains('show')) {
+        listOpen.classList.remove('show');
+      }
+    }
+  }
+}
