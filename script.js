e55df82c69f58da63a7d4f2d073bc0217a6cda1b
UmerZia <omerzia73@gmail.com>
2022-07-26 19:06:45 +0500

    add-js-for-hamburger

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function crossMenuFunc(x) {
-  x.classList.toggle("change");
+function hamburger () {
+  const hamburgerMainIcon = document.getElementsByClassName("hamburger-icon");
+  const hamburgerCrossIcon = document.getElementsByClassName("navbar__cross");
+  const hamburgerListItems = document.getElementsByClassName("navbar__list--item");
+
+  hamburgerMainIcon[0].addEventListener("click", () => {
+    document.getElementsByClassName("navbar__mobile__opt")[0].style.display = "flex";
+  });
+
+  hamburgerCrossIcon[0].addEventListener("click", () => {
+    document.getElementsByClassName("navbar__mobile__opt")[0].style.display = "none";
+  });
+
+  [...hamburgerListItems].forEach(item => {
+    item.addEventListener("click", ()=>{
+      document.getElementsByClassName("navbar__mobile__opt")[0].style.display = "none";
+    })
+  });
 }
 
-function dropdownFunc() {
-  document.getElementById("menuDrop").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.nav-menu')) {
-    let drpList = document.getElementsByClassName("drop-list");
-
-    for (let i = 0; i < drpList.length; i++) {
-      let listOpen = drpList[i];
-
-      if (listOpen.classList.contains('show')) {
-        listOpen.classList.remove('show');
-      }
-    }
-  }
-}
+hamburger();
